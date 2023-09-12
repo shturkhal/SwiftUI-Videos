589031f11c70c728d101b2519e51a2257e628d64
shturkhal <107053994+shturkhal@users.noreply.github.com>
2023-09-12 12:11:03 +0300

    Initial Commit

---
+++ SwiftUI-Videos/SwiftUI_VideosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_VideosApp.swift
+//  SwiftUI-Videos
+//
+//  Created by Іван Штурхаль on 12.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_VideosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
