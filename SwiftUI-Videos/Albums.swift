a765b8be71b8e176e2b8c44316e4f902568d9cc1
shturkhal <107053994+shturkhal@users.noreply.github.com>
2023-09-12 15:56:11 +0300

    add pic for album covers

---
+++ SwiftUI-Videos/Albums.swift
@@ -0,0 +1,33 @@
+//
+//  File.swift
+//  SwiftUI-Videos
+//
+//  Created by Іван Штурхаль on 12.09.2023.
+//
+
+import Foundation
+
+struct Album: Identifiable {
+    
+    var id = UUID()
+    var imageName: String
+    var artistName: String
+    var albumName: String
+}
+
+struct AlbumList {
+    static let topAlbums = [
+    Album(imageName: "Britney Spears - Lucky", artistName: "Britney Spears", albumName: "Lucky"),
+    Album(imageName: "Charlie Puth - Loser", artistName: "Charlie Puth", albumName: "Loser"),
+    Album(imageName: "Cher - Strong", artistName: "Cher", albumName: "Believe"),
+    Album(imageName: "Doja Cat - Woman", artistName: "Doja Cat", albumName: "Woman"),
+    Album(imageName: "Lady Gaga - Love game", artistName: "Lady Gaga", albumName: "Love game"),
+    Album(imageName: "Lana Del Rey - Cola", artistName: "Lana Del Rey", albumName: "Cola"),
+    Album(imageName: "Madonna - I don't give A", artistName: "Madonna", albumName: "MDNA"),
+    Album(imageName: "Madonna, Justin Timberlake - 4 minutes", artistName: "Madonna, Justin Timberlake", albumName: "4 minutes"),
+    Album(imageName: "Shakira - She wolf", artistName: "Shakira", albumName: "She wolf"),
+    Album(imageName: "Taylor Swift - Blank Space", artistName: "Taylor Swift", albumName: "Blank Space"),
+    Album(imageName: "Tove Lo - Borderline", artistName: "Tove Lo", albumName: "Borderline")
+    ]
+}
+
--- SwiftUI-Videos/ContentView.swift
+++ SwiftUI-Videos/ContentView.swift
@@ -8,19 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    var albums: [Album] = []
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List(albums) { album in
+                HStack {
+                    Image(album.imageName)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 90)
+                        .cornerRadius(8)
+                    
+                    Spacer().frame(width: 16)
+                    
+                    VStack(alignment: .leading) {
+                        
+                        Text (album.albumName)
+                            .fontWeight(.semibold)
+                        Spacer().frame(height: 5)
+                        Text(album.artistName)
+                            .font ( .subheadline)
+                            .foregroundColor (.secondary)
+                    }
+                }.navigationTitle("Albums")
+                
+                
+            }
+            
         }
-        .padding()
+        
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView(albums: AlbumList.topAlbums)
+        }
+        
     }
 }
